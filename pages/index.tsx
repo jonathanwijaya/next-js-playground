64dcc5104175b9c232868e0d4d03cd766c7d11ea
jonathanwijaya <jonathanwijaya961@gmail.com>
2022-05-29 10:53:20 +0700

    add tailwind scrollbar hide, add thumbnail component, finish row component,add tailwind custom scroll bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import Banner from "../components/Banner";
 import requests from "../utils/request";
 import { Movie } from "../typing";
+import Row from "../components/Row";
 
 interface Props {
   netflixOriginals: Movie[];
@@ -35,7 +36,16 @@ const Home = ({
       <Header />
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
         <Banner netflixOriginals={netflixOriginals} />
-        <section></section>
+        <section className="md:space-y-24">
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
+        </section>
       </main>
     </div>
   );
