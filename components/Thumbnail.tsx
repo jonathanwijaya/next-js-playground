5eee68e81474225b0f12c71b19eabf4f157b68a9
jonathanwijaya <jonathanwijaya961@gmail.com>
2022-06-16 15:57:46 +0700

    add rating into thumnail and preview title names

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -12,17 +12,26 @@ function Thumbnail({ movie }: Props) {
   const [showModal, setShowModal] = useRecoilState(modalState);
   const [currentMovie, setCurrentMovie] = useRecoilState(movieState);
 
+  console.log(movie)
+
   return (
     <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105"
       onClick={() => {
         setShowModal(true), setCurrentMovie(movie)
       }}>
+      <div className="relative h-full px-10 rounded-sm object-cover md:rounded transition duration-200 ease-out opacity-0 hover:opacity-100 z-50 justify-center items-center flex bg-black/80">
+        <span>{movie.original_title || movie.original_name}</span>
+
+      </div>
+      <div></div>
       <Image
-        src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path || movie.poster_path
-          }`}
+        src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path || movie.poster_path}`}
         className="rounded-sm object-cover md:rounded"
         layout="fill"
       />
+      <div className="absolute top-2 right-2 bg-red object-cover w-8 h-8 bg-red-900/80 shadow-2xl shadow-gray-500 rounded-full justify-center items-center flex">
+        {movie.vote_average}
+      </div>
     </div>
   );
 }
