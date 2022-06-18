6cf0da3e33fe99762fb6a2dc441ab3526709cf26
jonathanwijaya <jonathanwijaya961@gmail.com>
2022-06-19 06:35:42 +0700

    modify file name

--- ___tests___/thumbnail.test.tsx
+++ ___tests___/thumbnail.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
 import Thumbnail, { Props } from "../components/Thumbnail";
-import { mockData } from "../mock/mockData";
+import { mockData } from "../___mock___/mockData";
 import { RecoilRoot } from "recoil";
 import Image from "next/image";
 
